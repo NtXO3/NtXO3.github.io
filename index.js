30c48ef884d314128101ba755533ae5f694040be
Nathan Minkhorst <nathanminkhorst@gmail.com>
2022-06-14 23:29:18 +0200

    waves

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let isModalOpen = false;
 let isDarkTheme = JSON.parse(localStorage.getItem('dark')) === true
 const scaleFactor = 1 / 20;
 const contrastToggleEl = document.querySelector('#contrast-toggle')
+const allSectionElements = document.querySelectorAll('section')
+const navDotsEl = document.querySelectorAll('.nav__dot')
 
 function moveBackground(event) {
     const shapes = document.querySelectorAll(".shape")
@@ -19,6 +21,17 @@ function moveBackground(event) {
         const booleanInt = isOdd ? -1 : 1;
         shapes[i].style.transform = `translate(${x * booleanInt}px, ${y *booleanInt}px)`
     }
+}
+
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect()
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
 }
 
 
