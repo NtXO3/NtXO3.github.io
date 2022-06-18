b11f0722d60e3de7a140e556a285546dee3a89fc
Nathan Minkhorst <nathanminkhorst@gmail.com>
2022-06-18 15:22:46 +0200

    typing animation

--- index.js
+++ index.js
@@ -10,6 +10,39 @@ const scaleFactor = 1 / 20;
 const contrastToggleEl = document.querySelector('#contrast-toggle')
 const allSectionElements = document.querySelectorAll('section')
 const navDotsEl = document.querySelectorAll('.nav__dot')
+const titleEl = document.querySelector('.title')
+const titleColorEl = document.querySelector('.title__color')
+const typingCursorEl = document.querySelector('.typing__cursor')
+
+async function typingTitle() {
+    const titleTop = ['H', 'e', 'y', '!', `<br>`]
+    const titleBottom = 'Ik ben Nathan.'
+    let timeoutIndex = 1
+    for (let i = 0; i < titleTop.length; ++i) {
+        setTimeout(() => {
+            titleEl.innerHTML += titleTop[i]
+        }, 180 * timeoutIndex)
+        timeoutIndex += 1
+        
+    }
+
+    setTimeout(() => {
+        if (isDarkTheme) {
+            typingCursorEl.style.color = 'rgb(106, 228, 255)'
+            return
+        }
+        typingCursorEl.style.color = 'rgb(122, 89, 255)'
+    }, 180 * timeoutIndex)
+
+    for (let i = 0; i < titleBottom.length; ++i) {
+        setTimeout(() => {
+            titleColorEl.innerText += titleBottom[i]
+        }, 180 * timeoutIndex)
+        timeoutIndex += 1
+    }
+}
+
+typingTitle()
 
 function moveBackground(event) {
     const shapes = document.querySelectorAll(".shape")
@@ -44,6 +77,7 @@ function toggleContrast(event) {
         localStorage.setItem('dark', true)
         contrastToggleEl.innerHTML = `<i class="fas fa-sun"></i>`
         contrastToggleEl.childNodes[0].classList += ' rotate'
+        typingCursorEl.style.color = 'rgb(106, 228, 255)'
         setTimeout(() => {
             contrastToggleEl.childNodes[0].classList.remove('rotate')
         }, 500)
@@ -53,6 +87,7 @@ function toggleContrast(event) {
         localStorage.setItem('dark', false)
         contrastToggleEl.innerHTML = `<i class="fas fa-moon"></i>`
         contrastToggleEl.childNodes[0].classList += ' rotate'
+        typingCursorEl.style.color = 'rgb(122, 89, 255)'
         setTimeout(() => {
             contrastToggleEl.childNodes[0].classList.remove('rotate')
         }, 500)
@@ -97,4 +132,3 @@ function toggleModal() {
     //toggle modal
     document.body.classList += " modal--open"
 }
-
